ea58633ccb12d9f86442409574eeab20c0c9d989
DavidChu <xiaoshuai.chu@mail.utoronto.ca>
2023-09-11 20:54:00 -0400

    changes to activities 1

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,38 +1,34 @@
 /**
  * Solve the FizzBuzz challenge.
  */
-class FizzBuzz {
-
+public class FizzBuzz {
     public static void main(String[] args) {
 
-        for (int i = 1; i < 100; i++) {
+        int i = 1;
 
-            doFizzBuzz(i);
+        while (i < 100) {
+
+            i = doFizzBuzz(i);
         }
     }
 
-    private static void doFizzBuzz(int i) {
-        // Find out which numbers divide i.
+    public static int doFizzBuzz(int i) {
+
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
 
-        // Print our appropriate result.
         if (divisibleBy3 && divisibleBy5) {
-
             System.out.println("Fizz Buzz");
-
         } else if (divisibleBy3) {
-
             System.out.println("Fizz");
-
         } else if (divisibleBy5) {
-
             System.out.println("Buzz");
-
         } else {
-
             System.out.println(i);
-
         }
+
+        ++i;
+        return i;
     }
 }
+
